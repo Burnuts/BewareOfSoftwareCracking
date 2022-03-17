e06d65763937828b63021dc4d562691cf0fc4324
GitHub Action <action@github.com>
2022-03-17 17:32:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/burnuts/bewareofsoftwarecracking/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.burnuts.bewareofsoftwarecracking.services
+
+import com.github.burnuts.bewareofsoftwarecracking.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/burnuts/bewareofsoftwarecracking/services/MyProjectService.kt
+++ src/main/kotlin/com/github/burnuts/bewareofsoftwarecracking/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.burnuts.bewareofsoftwarecracking.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.burnuts.bewareofsoftwarecracking.MyBundle
 
 class MyProjectService(project: Project) {
 
